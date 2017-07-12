75e522827521702caebc289032972f094c0dc95c
良隆　川部 <yoshi2260027@gmail.com>
2017-07-12 12:49:57 +0900

    音出すときに音量が低いと嫌だから勝手に2秒で音量を４０くらいあげる機能を実装しました

--- HelloProject/src/jp/co/agilegroup/sample/HelloServlet.java
+++ HelloProject/src/jp/co/agilegroup/sample/HelloServlet.java
@@ -52,11 +52,24 @@ public class HelloServlet extends HttpServlet {
 		out.close();
 		try {
 			rb = new Robot();
-			rb.setAutoDelay(100);
+			rb.keyPress(KeyEvent.VK_WINDOWS);
+			rb.delay(500);
+			rb.keyPress(KeyEvent.VK_5);
+			rb.delay(1);
+			rb.keyRelease(KeyEvent.VK_5);
+			rb.keyRelease(KeyEvent.VK_WINDOWS);
+
+			for (int i = 0; i < 40; i++) {
+				rb.keyPress(KeyEvent.VK_UP);
+				rb.keyRelease(KeyEvent.VK_UP);
+				rb.delay(50);
+			}
+
+			rb.setAutoDelay(200);
 			rb.keyPress(KeyEvent.VK_WINDOWS);
 			rb.delay(500);
 			rb.keyPress(KeyEvent.VK_D);
-			rb.delay(1000);
+			rb.delay(1);
 			rb.keyRelease(KeyEvent.VK_D);
 			rb.keyRelease(KeyEvent.VK_WINDOWS);
 		} catch (AWTException e) {
@@ -72,41 +85,41 @@ public class HelloServlet extends HttpServlet {
 				u = (int) (Math.random() * 1500);
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				JPanel p = new JPanel();
-				 p.setBackground(Color.RED);
+				p.setBackground(Color.RED);
 				p.setLayout(null);
 				//
 				JButton button = new JButton("閉じる");
 				button.setBounds(420, 250, 80, 30);
 				p.add(button);
 				//
-				JLabel label=new JLabel("システムはシャットダウンされます。進行中の作業をすべて保存し、");
-				label.setBounds(45,100,460,20);
+				JLabel label = new JLabel("システムはシャットダウンされます。進行中の作業をすべて保存し、");
+				label.setBounds(45, 100, 460, 20);
 				p.add(label);
-				Font g = new Font("Serif",Font.PLAIN,14);
+				Font g = new Font("Serif", Font.PLAIN, 14);
 				label.setFont(g);
 				//
-				JLabel label2=new JLabel("ログオフしてください。保存されていないデータは失われます。");
-				label2.setBounds(45,120,460,20);
+				JLabel label2 = new JLabel("ログオフしてください。保存されていないデータは失われます。");
+				label2.setBounds(45, 120, 460, 20);
 				p.add(label2);
-				Font h = new Font("Serif",Font.PLAIN,14);
+				Font h = new Font("Serif", Font.PLAIN, 14);
 				label2.setFont(h);
 				//
-				JLabel label3=new JLabel("シャットダウンは、NTAUTHORITY￥SYSTEMによって開始されました");
-				label3.setBounds(45,140,460,20);
+				JLabel label3 = new JLabel("シャットダウンは、NTAUTHORITY￥SYSTEMによって開始されました");
+				label3.setBounds(45, 140, 460, 20);
 				p.add(label3);
-				Font i = new Font("Serif",Font.PLAIN,14);
+				Font i = new Font("Serif", Font.PLAIN, 14);
 				label3.setFont(i);
 				//
-				JLabel label4=new JLabel("=============警告==============");
-				label4.setBounds(10,10,550,30);
-				Font f = new Font("Serif",Font.PLAIN,30);
+				JLabel label4 = new JLabel("=============警告==============");
+				label4.setBounds(10, 10, 550, 30);
+				Font f = new Font("Serif", Font.PLAIN, 30);
 				label4.setFont(f);
 				p.add(label4);
 				Container contentPane = frame.getContentPane();
-			    contentPane.add(p, BorderLayout.CENTER);
-			    java.awt.Toolkit.getDefaultToolkit().beep();
+				contentPane.add(p, BorderLayout.CENTER);
+				java.awt.Toolkit.getDefaultToolkit().beep();
 
-			    frame.getContentPane().add(p, BorderLayout.CENTER);
+				frame.getContentPane().add(p, BorderLayout.CENTER);
 				frame.setSize(550, 350);
 				frame.setVisible(true);
 				frame.setBounds(u, y, 550, 350);
